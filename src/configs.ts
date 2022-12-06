018e9883c52a9805d2a038a9be7116a9d98d972c
Guillermo Morton <guillermo.morton@possumus.tech>
2022-12-06 10:16:27 -0300

    Se configuro las migraciones, se genera un usuario por defecto

--- src/configs.ts
+++ src/configs.ts
@@ -6,7 +6,17 @@ const configs = {
         uri: process.env.URI,
         default_page_count: process.env.DEFAULT_PAGE_COUNT,
     },
-    secret_key: process.env.SECRETPRIVATEKEY
+    secret_key: process.env.SECRETPRIVATEKEY,
+    user_default: {
+        name: process.env.MIGRATE_USER_DEFAULT_NAME,
+        email: process.env.MIGRATE_USER_DEFAULT_EMAIL,
+        password: process.env.MIGRATE_USER_DEFAULT_PASSWORD,
+    },
+    migrations: {
+        uri: process.env.MIGRATE_MONGO_URI,
+        db: process.env.MIGRATE_MONGO_DB
+
+    }
 
 }
 
