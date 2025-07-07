db01ab819ce9c0dbaaa30485939a5688e6b4a727
Gianella Vezzoni <gianella.vezzoni@possumus.tech>
2025-07-07 07:43:46 -0300

    fixes

--- src/configs.ts
+++ src/configs.ts
@@ -1,23 +1,21 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const configs = {
-    api: {
-        port: process.env.PORT,
-        uri: process.env.URI,
-        default_page_count: process.env.DEFAULT_PAGE_COUNT,
-    },
-    secret_key: process.env.SECRETPRIVATEKEY,
-    user_default: {
-        name: process.env.MIGRATE_USER_DEFAULT_NAME,
-        email: process.env.MIGRATE_USER_DEFAULT_EMAIL,
-        password: process.env.MIGRATE_USER_DEFAULT_PASSWORD,
-    },
-    migrations: {
-        uri: process.env.MIGRATE_MONGO_URI,
-        db: process.env.MIGRATE_MONGO_DB
+  api: {
+    port: process.env.PORT,
+    uri: process.env.MONGO_CONNECTION_URI,
+    default_page_count: process.env.DEFAULT_PAGE_COUNT,
+  },
+  secret_key: process.env.SECRETPRIVATEKEY,
+  user_default: {
+    name: process.env.MIGRATE_USER_DEFAULT_NAME,
+    email: process.env.MIGRATE_USER_DEFAULT_EMAIL,
+    password: process.env.MIGRATE_USER_DEFAULT_PASSWORD,
+  },
+  migrations: {
+    uri: process.env.MIGRATE_MONGO_URI,
+    db: process.env.MIGRATE_MONGO_DB,
+  },
+};
 
-    }
-
-}
-
-export default configs
+export default configs;
