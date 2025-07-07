c22dc29cc568751577c0a95aa261accca26e2604
Gianella Vezzoni <gianella.vezzoni@possumus.tech>
2025-07-07 09:24:00 -0300

    migrations modified

--- src/modules/statusCheck/index.ts
+++ src/modules/statusCheck/index.ts
@@ -1,12 +1,17 @@
 import { Application } from "express";
-import { mongoMigrateCli } from "mongo-migrate-ts";
 
 export const RunMigrations = (app: Application) => {
   app.post("/admin/migrate", async (req, res) => {
+    if (req.headers.authorization !== `${process.env.ADMIN_KEY}`) {
+      return res.status(403).send("Unauthorized");
+    }
+
     try {
-      await mongoMigrateCli();
+      const migrate = require("../../../build/migrations/index.js");
+      await migrate.up();
       res.send("✅ Migraciones ejecutadas");
     } catch (err) {
+      console.error(err);
       res.status(500).send("❌ Error al ejecutar migraciones");
     }
   });
