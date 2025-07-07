9c59f1caf9daec80d7c989172197e4d317aec032
Gianella Vezzoni <gianella.vezzoni@possumus.tech>
2025-07-07 07:33:23 -0300

    seeder fixed

--- src/seeders/userSeeder.ts
+++ src/seeders/userSeeder.ts
@@ -1,20 +1,26 @@
-import { DependencyManager } from "../dependencyManager";
 import { MongoUserRepository } from "../modules/users/infrastructure/repository/MongoUserRepository";
 import { BcryptHashService } from "../modules/users/infrastructure/services/BcryptHashService";
 import { SaveUserAction } from "../modules/users/core/actions/SaveUserAction";
+import { MongoProgressRepository } from "../modules/progress/infrastructure/repository/MongoProgressRepository";
 
 const seedGenericUser = async () => {
   try {
     const userRepository = MongoUserRepository();
     const hashService = BcryptHashService();
-    const saveUserAction = SaveUserAction(userRepository, hashService);
+    const progressRepository = MongoProgressRepository();
+    const saveUserAction = SaveUserAction(
+      userRepository,
+      hashService,
+      progressRepository
+    );
 
     const genericUser = {
       name: "Generic User",
-      email: "mail@test.com",
+      email: "gianella.vezzoni@possumus.tech",
       password: "123456",
-      role: "ADMIN_ROLE",
+      role: "SUPER_ADMIN_ROLE",
       status: true,
+      objectives: [],
     };
 
     const result = await saveUserAction.execute(genericUser);
