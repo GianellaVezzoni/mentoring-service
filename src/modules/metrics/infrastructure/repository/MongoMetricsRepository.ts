82d320ac693bbd191b50c77910575320292b0439
Gianella Vezzoni <48287696+GianellaVezzoni@users.noreply.github.com>
2025-07-08 16:20:47 -0300

    Update MongoMetricsRepository.ts

--- src/modules/metrics/infrastructure/repository/MongoMetricsRepository.ts
+++ src/modules/metrics/infrastructure/repository/MongoMetricsRepository.ts
@@ -13,25 +13,7 @@ export const MongoMetricsRepository = (): IMetricRepository => ({
     return await MetricModel.findByIdAndDelete(id);
   },
   async get(query) {
-    const {
-      page_count = configs.api.default_page_count,
-      page_number = 0,
-      ...rest
-    } = query;
-    const total = await MetricModel.countDocuments(rest);
-    const metrics = await MetricModel.find(rest)
-      .limit(Number(page_count))
-      .skip(Number(page_number));
-    const pagination = {
-      total,
-      page_number,
-      page_count,
-      records: metrics.length,
-    };
-    return {
-      metrics,
-      pagination,
-    };
+    return await MetricModel.find(query);
   },
   async getById(id) {
     return await MetricModel.findById(id);
