fc59405b1b9cccb47cb769cea2c0bc779c55a1b2
Gianella Vezzoni <gianella.vezzoni@possumus.tech>
2025-07-07 23:08:22 -0300

    fixes

--- src/server/ServerInitializer.ts
+++ src/server/ServerInitializer.ts
@@ -4,7 +4,6 @@ const express = require("express");
 const InitializeServer = () => {
   const app = express();
   app.set("port", configs.api.port);
-  console.log("app.get(", app.get("port"));
   app.listen(app.get("port"), () => {
     console.log("Servidor iniciado, puerto " + app.get("port"));
   });
