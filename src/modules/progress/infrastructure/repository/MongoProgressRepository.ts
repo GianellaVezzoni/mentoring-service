042817a53aa86fcd1e4ab1d9b1e6aada8d5ef041
Gianella Vezzoni <48287696+GianellaVezzoni@users.noreply.github.com>
2025-07-08 16:22:04 -0300

    Update MongoProgressRepository.ts

--- src/modules/progress/infrastructure/repository/MongoProgressRepository.ts
+++ src/modules/progress/infrastructure/repository/MongoProgressRepository.ts
@@ -14,25 +14,7 @@ export const MongoProgressRepository = (): IProgressRepository => ({
     return await ProgressModel.findByIdAndDelete(id);
   },
   async get(query) {
-    const {
-      page_count = configs.api.default_page_count,
-      page_number = 0,
-      ...rest
-    } = query;
-    const total = await ProgressModel.countDocuments(rest);
-    const progress = await ProgressModel.find(rest)
-      .limit(Number(page_count))
-      .skip(Number(page_number));
-    const pagination = {
-      total,
-      page_number,
-      page_count,
-      records: progress.length,
-    };
-    return {
-      progress,
-      pagination,
-    };
+    return await ProgressModel.find(query);
   },
   async getById(id) {
     return await ProgressModel.findById(id);
